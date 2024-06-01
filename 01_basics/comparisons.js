7d53b32909316bca944a833461e4d448a4458c16
Md Abdullah Al Noman <92081342+Md-AbdullahAl-Noman@users.noreply.github.com>
2024-06-01 18:25:59 +0000

    comparisons added

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,24 @@
+console.log(2==2)
+console.log(2>=5)
+console.log(2>=1)
+console.log(2<=1)
+console.log(12<11)
+// diff data types
+console.log("After same types");
+console.log("5">2)
+console.log("5">"2")
+console.log("Noman">"Das")
+console.log("Das">"Noman")
+
+//importants and tricky
+console.log("After different types");
+console.log(null==0)
+console.log(null==1)
+console.log(null>=1)
+console.log(null<=1)
+console.log(null<1)
+//undefined comparisons
+console.log("Undefined types");
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined>=0);
