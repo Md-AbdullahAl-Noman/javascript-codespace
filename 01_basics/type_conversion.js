47102ecad5005e630ccac527cdefea7458452266
Md Abdullah Al Noman <92081342+Md-AbdullahAl-Noman@users.noreply.github.com>
2024-06-01 18:00:06 +0000

    type_conversion added

---
+++ 01_basics/type_conversion.js
@@ -0,0 +1,20 @@
+let checkBool=""
+
+console.log(typeof checkBool)
+
+let isLogged=false
+let boolToNumber=Number(isLogged)
+console.log(typeof boolToNumber);
+console.log(boolToNumber)
+// number conversion
+let anyNumber="33"
+let toNumber=Number(anyNumber)
+console.log(toNumber);
+console.log(typeof toNumber);
+// numberwithchar
+let testNumber="33abc"
+let testToNumber=Number(testNumber)
+console.log(testToNumber);
+console.log(typeof testToNumber);
+
+console.log("Here the value is:"+  +false)
