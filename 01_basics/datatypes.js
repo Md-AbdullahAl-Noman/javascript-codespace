77f92bc54084f5ef4cc27f5f4026e3537cada2da
Md Abdullah Al Noman <92081342+Md-AbdullahAl-Noman@users.noreply.github.com>
2024-06-01 16:58:37 +0000

    datatypes added

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+// symbols for individual components or say uniqueness
+//null is a standalone data types which is object
+//undefined means it is not initialized with value
+// boolean => true/false
+
+let age=20
+let isActive=false
+let nameIs="Noman"
+let type;
+let whatToDO
+
+console.table([age,isActive,nameIs,type,whatToDO])
+console.log(typeof "Rony")
+console.log(typeof undefined)
+console.log(typeof null)
---
+++ 01_basics/variables_name.js
@@ -0,0 +1,13 @@
+console.log("Noman")
+
+const myId = 44379
+let myGender= "Male"
+var house = "House#83"
+let accountState;
+
+noVariable = "Dhaka"
+
+// myId= 2121;can not reassigned
+house="Tongi#83"
+// avoid use of var as it lacks block scoped problem
+console.table([myId,myGender,house,noVariable,accountState])
