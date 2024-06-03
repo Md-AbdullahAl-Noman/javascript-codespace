82cfa171e0c2ffc3c60ba9c8ea5500437254c0ce
Md Abdullah Al Noman <92081342+Md-AbdullahAl-Noman@users.noreply.github.com>
2024-06-03 09:08:22 +0000

    string operations

---
+++ 01_basics/strings.js
@@ -0,0 +1,19 @@
+let firstString="Code my strings"
+console.log(firstString.lastIndexOf('ode'))
+console.log(firstString.toUpperCase())
+console.log(firstString.toLowerCase())
+
+console.log(`The console is for ${firstString} to use backticks as it is useful for placeholders and string interpolation`)   
+
+let secondString= new String("Enjoying Javascript")
+// includes a substring or not AND other methods
+console.log(secondString.includes("ing"))
+console.log(secondString.indexOf("ipt"))
+console.log(secondString.charAt(5))
+console.log(secondString.replace("Enjoying","Excellent"))
+console.log(secondString.split(" "))
+console.log(secondString.slice(-5,-3))
+// console.log(secondString)
+
+
+
