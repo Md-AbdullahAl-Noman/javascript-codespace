d3fe36b438ccb25e063908e559a4d8ab9d264b85
Md Abdullah Al Noman <92081342+Md-AbdullahAl-Noman@users.noreply.github.com>
2024-06-04 10:25:53 +0000

    object part2 added

---
+++ 02_basics/objects_02.js
@@ -0,0 +1,55 @@
+// const objSingleton = new Object()
+// console.log(objSingleton)
+
+const userObj ={
+    fullname:{
+        name:{
+            firstname:"Md Adullah Al",
+            "last name":"Noman"
+        }
+    }
+
+
+}
+// console.log(userObj);
+// console.log(userObj.fullname);
+// console.log(userObj.fullname.name.firstname);
+ 
+//objects 
+const obj1={
+    name:"Ali",
+    state:false
+}
+
+const obj2={}
+obj2.id="101"
+obj2.Loggedstate=true
+
+console.log(obj2);
+const obj3={obj1,obj2}
+console.log(obj3);
+//use of assign to merge objects
+const obj4=Object.assign({},obj1,obj2)
+console.log(obj4);
+
+//array of objects
+
+const userList = [
+    {
+        name:"Noman",
+        id:1
+
+    },
+    {
+        name:"Das",
+        id:2
+
+    }
+
+]
+
+console.log(userList[1].name)
+
+console.log(Object.keys(obj1))
+console.log(Object.values(obj1))
+console.log(obj1.hasOwnProperty("name"))//to check a property
