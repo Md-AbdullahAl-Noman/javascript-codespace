0e7be837751c1cdaed94a3926935405527907c7e
Md Abdullah Al Noman <92081342+Md-AbdullahAl-Noman@users.noreply.github.com>
2024-06-07 13:32:08 +0000

    while and do while added

---
+++ 05_loops/while_do_while.js
@@ -0,0 +1,26 @@
+//while (condition) {execution}
+
+let index = 0;
+while(index<=6)
+    {
+        // console.log(index);
+        //to terminate the loop the increment and decrement is must
+        index++
+    }
+
+let indexDec = 10;
+while(indexDec >6)
+    {
+        // console.log(indexDec);
+        //to terminate the loop the increment and decrement is must
+        indexDec--
+    }
+
+    //do while
+    let doVar=5
+
+    do{
+        //alwys execute this block
+        console.log(doVar);
+        doVar++;
+    }while(doVar<=4)//false but the do will execute that code
